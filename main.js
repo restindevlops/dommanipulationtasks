219cc6af115991b4c6992617b05f57c2e8ad741c
restindevlops <restincraji@gmail.com>
2023-02-08 21:49:04 +0530

    getelmntby classname

--- main.js
+++ main.js
@@ -3,9 +3,7 @@
 var HeaderTitle=document.getElementById('header-title');
 
 HeaderTitle.textContent='Item Display';
-
 HeaderTitle.innerText="Item Lister";
-
 HeaderTitle.style.color="white";
 HeaderTitle.style.fontFamily='segoe ui';
 HeaderTitle.innerHTML="<h1> Display Items</h1>";
@@ -14,11 +12,19 @@ var Border=document.getElementById('main-header');
 Border.style.borderBottom='solid 10px orange';
 
 //GETELEMENTBYCLASSNAME
+
 FormTitle=document.getElementsByClassName('form-title');
-console.log(FormTitle);
+
 FormTitle[0].style.fontWeight='bold';
 FormTitle[0].style.color='green';
-//FormTitle.style.fontWeight='bold';
-//FormTitle.style.color='green';
+
+var Items=document.getElementsByClassName('list-group-item');
+console.log(Items);
+Items[2].style.backgroundColor='lightgreen';
+
+for( var i=0;i<Items.length;i++)
+{
+    Items[i].style.fontWeight='bold';
+}
 
 
