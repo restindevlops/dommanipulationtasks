1df956ce204121dcac6944cbe5e53be521af3ec2
restindevlops <restincraji@gmail.com>
2023-02-08 22:50:56 +0530

    queryselector

--- main.js
+++ main.js
@@ -35,10 +35,41 @@
 
 //GETELEMENTSBYTAGNAME
 
-var li=document.getElementsByTagName('li');
-li[1].style.fontWeight='bold';
+// var li=document.getElementsByTagName('li');
+// li[1].style.fontWeight='bold';
 
-for(var i=0;i<li.length;i=i+2)
-{
-    li[i].style.backgroundColor='lightgreen'
-}
+// for(var i=0;i<li.length;i=i+2)
+// {
+//     li[i].style.backgroundColor='lightgreen'
+// }
+
+//QUERYSELECTOR
+  
+
+  // 1. Using idname
+var item=document.querySelector('#main-header')
+item.style.borderBottom='solid 10px orange'
+
+  // 2. using tagname 
+var input=document.querySelector('input')
+input.value="Item 5";
+
+  // 3. for next elemnt with same tagname
+var submit=document.querySelector('input[type="submit"]')
+submit.value="SEND";
+
+  // 4.Using Classname for last child
+ var lastelem=document.querySelector('.list-group-item:last-child') 
+ lastelem.textContent=" Item 4";
+
+  // 5.Using Classname for nth child
+  var lastelem=document.querySelector('.list-group-item:nth-child(3)') 
+  lastelem.textContent=" Item 3";
+
+    // 6.Using Classname for nth child
+    var lastelem=document.querySelector('.list-group-item:nth-child(2)') 
+    lastelem.style.backgroundColor=" lightgreen";
+  
+    // 7.Using Classname for nth child
+  var lastelem=document.querySelector('.list-group-item:nth-child(3)') 
+  lastelem.style.display=" none";  
